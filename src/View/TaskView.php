8ca786159bbccc1feffc359d904debe9c1f72c93
tiagoramos1082 <tiagoramos1082@gmail.com>
2026-09-24 13:13:40 -0300

    fix: add the missing (\n)

--- src/View/TaskView.php
+++ src/View/TaskView.php
@@ -31,7 +31,7 @@ class TaskView
 
         print("1. CRIAR TAREFA\n");
         print("2. ALTERNAR STATUS\n");
-        print("3. ALTERAR NOME DA TAREFA");
+        print("3. ALTERAR NOME DA TAREFA\n");
         print("4. DELETAR TAREFA\n");
         print("0. SAIR\n");
 
